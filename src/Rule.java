72702f489029a505d0cd87008bd692d076348f9a
bfid111 <bfid111@gmail.com>
2016-05-28 13:45:21 -0400

    Added sample

--- src/Rule.java
+++ src/Rule.java
@@ -1,4 +1,34 @@
-
 public abstract class Rule {
+
+    // Represents a schedule that is impossible to
+    // realize. For example, an instructor scheduled
+    // for two simultaneous classes.
+    //
+    public final static int IMPOSSIBLE = 1000000;
+
+    // This operation returns a fitness score for the
+    // provided schedule. Higher values indicate a less
+    // fit schedule. A value of 0 indicates a perfect fit.
+    //
+    public int getFitness(Schedule schedule); 
+
+}
+public class SpecificRule extends Rule {
+
+    // This operation returns a fitness score for the
+    // provided schedule. Higher values indicate a less
+    // fit schedule. A value of 0 indicates a perfect fit.
+    //
+    public int getFitness(Schedule schedule); 
+}
+public class InstructorConflict implements Rule {
+    
+    // This operation returns 0 if no instructors
+    // are scheduled for simultaneous classes and
+    // IMPOSSIBLE otherwise.
+    //
+    public int getFitness(Schedule schedule) {
+        ...
+    }
 
 }
