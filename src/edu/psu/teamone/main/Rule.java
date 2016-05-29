28d22320d369b5960c762fdcaf13a3ae09928e1f
bfid111 <bfid111@gmail.com>
2016-05-28 21:31:24 -0400

    Added CheckConflicts(works with only hours so far)

--- src/edu/psu/teamone/main/Rule.java
+++ src/edu/psu/teamone/main/Rule.java
@@ -1,5 +1,9 @@
 package edu.psu.teamone.main;
 
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
 public abstract class Rule {
 
 	// Represents a schedule that is impossible to
@@ -22,11 +26,45 @@ public abstract class Rule {
 	}
 
 	public class InstructorConflict extends Rule {
+		@Override
 		// This operation returns 0 if no instructors
 		// are scheduled for simultaneous classes and
 		// IMPOSSIBLE otherwise.
 		public int getFitness(Schedule schedule) {
-			return 0;
+			ArrayList<Meeting> meetingList = new ArrayList<Meeting>();
+			for (Entry<Section, Meeting> scheduleEntry : schedule.getSections().entrySet()) {
+				// Put all meeting in schedule to meetingList
+				meetingList.add(scheduleEntry.getValue());
+			}
+			boolean checkConflicts = checkConflicts(meetingList);
+			return checkConflicts ? IMPOSSIBLE : 0;
+		}
+
+		@SuppressWarnings("deprecation") // TODO: Use Calendar Class
+		private boolean checkConflicts(ArrayList<Meeting> meetingList) {
+			// Checks if there is any conflicting class schedule
+			// Condition: Schedule 1 starts on or before Schedule 2 and Schedule
+			// 1 Ends after Schedule 2 Starts
+			// TODO: Use Calendar and include minutes
+			for (int i = 0; i < meetingList.size(); i++) {
+				// Go through all meetings and check any time conflicts
+				for (int j = i + 1; j < meetingList.size() - 1; j++) {
+					// Compare each meeting to all other meetings
+					if (meetingList.get(i).getDays() == meetingList.get(j).getDays()) {
+						// Class on same days
+						int compHourOneStart = meetingList.get(i).getStartTime().getHours();
+						int compHourOneEnd = meetingList.get(i).getStopTime().getHours();
+						int compHourTwoStart = meetingList.get(j).getStartTime().getHours();
+						if (compHourOneStart <= compHourTwoStart && compHourOneEnd > compHourTwoStart) {
+							// If one class starts before other class starts and
+							// ends after that class starts
+							// It means that the schedule conflicts.
+							return false;
+						}
+					}
+				}
+			}
+			return true;
 		}
 	}
 }
