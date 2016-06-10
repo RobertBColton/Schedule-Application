8cab6ab2de22d5318f38e83f62d3a316cd252047
RobertBColton <robertbcolton@gmail.com>
2016-06-10 04:04:59 -0400

    Sets up JavaFX

--- src/edu/psu/teamone/main/ScheduleApplication.java
+++ src/edu/psu/teamone/main/ScheduleApplication.java
@@ -1,13 +1,27 @@
 package edu.psu.teamone.main;
 
-import javax.swing.JFrame;
+import java.io.IOException;
+import java.util.ResourceBundle;
 
-public class ScheduleApplication {
-	private static JFrame mainFrame;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ScheduleApplication extends Application {
 
 	public static void main(String[] args) {
-		mainFrame = new JFrame();
-		mainFrame.setVisible(true);
-		mainFrame.setSize(800, 600); // Change this in the future.
+		launch(args);
+	}
+
+	@Override
+	public final void start(Stage primaryStage) throws IOException {
+		final Parent root = FXMLLoader.load(getClass().getResource("main.fxml"),
+				ResourceBundle.getBundle("edu.psu.teamone.main.messages"));
+
+		primaryStage.setTitle("Urika");
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
 	}
 }
