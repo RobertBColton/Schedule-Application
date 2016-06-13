e2521738a370770970e3c40b5aa87ee5be18d5e0
RobertBColton <robertbcolton@gmail.com>
2016-06-12 21:18:40 -0400

    Cleanup Simpler method for launching the default browser. Fixed duplication of layout.

--- src/edu/psu/teamone/main/ScheduleApplication.java
+++ src/edu/psu/teamone/main/ScheduleApplication.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.application.HostServices;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,12 +12,20 @@ import javafx.stage.Stage;
 
 public class ScheduleApplication extends Application {
 
+	private static HostServices hostServices;
+
+	public static HostServices getStaticHostServices() {
+		return hostServices;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public final void start(Stage primaryStage) throws IOException {
+		hostServices = this.getHostServices();
+
 		final Parent root = FXMLLoader.load(getClass().getResource("main.fxml"),
 				ResourceBundle.getBundle("edu.psu.teamone.main.messages"));
 
