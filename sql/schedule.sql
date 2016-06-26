af1f184a85ff0e3051a6247e1a0ae4989cea8bc8
1nz01111 <bfid111@gmail.com>
2016-06-26 18:43:18 -0400

    Update schedule.sql

--- sql/schedule.sql
+++ sql/schedule.sql
@@ -1,21 +1,19 @@
 ﻿CREATE DATABASE IF NOT EXISTS schedule;
 USE schedule;
 
-/*instructors*/
-DROP TABLE IF EXISTS `instructor`;
-CREATE TABLE `instructor` (
-  `instructor id` int(64) NOT NULL,
-  `first name` text NOT NULL,
-  `last name` text NOT NULL,
-  PRIMARY KEY (`instructor id`)
-);
-
 /*classes*/
 DROP TABLE IF EXISTS `classes`;
 CREATE TABLE `classes` (
   `course id` int(64) NOT NULL,
   `course name` text NOT NULL,
   `course abbreviation` text NOT NULL,
+  PRIMARY KEY (`course id`)
+);
+
+/*meetings*/
+DROP TABLE IF EXISTS `meetings`;
+CREATE TABLE `meetings` (
+  `meeting id` int(64) NOT NULL,
   `start time` time NOT NULL,
   `end time` time NOT NULL,
   `M` bool null,
@@ -23,19 +21,15 @@ CREATE TABLE `classes` (
   `W` bool null,
   `TR` bool null,
   `F` bool null,
-  PRIMARY KEY (`course id`)
+  PRIMARY KEY (`meeting id`),
 );
 
-/*schedules*/
-DROP TABLE IF EXISTS `schedules`;
-CREATE TABLE `schedules` (
-  `schedule id` int(64) NULL,
-  `instructor id` int(64)NULL,
-  `course #1 id` int(64) NULL,
-  `course #2 id` int(64) NULL,
-  `course #3 id` int(64) NULL,
-  `course #4 id` int(64) NULL,
-  `course #5 id` int(64) NULL,
-  `course #6 id` int(64) NULL,
-  PRIMARY KEY(`schedule id`)
+/*classmeetings*/
+DROP TABLE IF EXISTS `classmeetings`;
+CREATE TABLE `classmeetings` (
+  `meeting id` int(64) NOT NULL,
+  `course id` int(64) NOT NULL,
+  FOREIGN KEY (`meeting id`) REFERENCES meetings(`meeting id`),
+  FOREIGN KEY (`course id`) REFERENCES classes(`course id`)
+  PRIMARY KEY (`meeting id`, `course id`),
 );
