5102b57fbe915a3382745982cd5038efe4d08dfc
bfid111 <bfid111@gmail.com>
2016-07-06 16:00:26 -0400

    add addMeeting

--- src/edu/psu/teamone/main/Database.java
+++ src/edu/psu/teamone/main/Database.java
@@ -28,6 +28,53 @@ public class Database {
 					courseAbb);
 			st.executeUpdate(command);
 			st.close();
+
+		} catch (SQLException ex) {
+
+			Logger lgr = Logger.getLogger(Database.class.getName());
+			lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+		} finally {
+			// close db connection
+			try {
+
+				if (rs != null) {
+					rs.close();
+				}
+
+				if (st != null) {
+					st.close();
+				}
+
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException ex) {
+
+				Logger lgr = Logger.getLogger(Database.class.getName());
+				lgr.log(Level.WARNING, ex.getMessage(), ex);
+			}
+		}
+	}
+
+	public void addMeeting(int meetingId, String startTime, String endTime, String days) {
+		// course Id and meeting Id are same, thus meeting 0000001 means it
+		// teaches course 0000001
+		// days indicate which days the meeting is held. "00101" means
+		// Wednesdays and Fridays
+		try {
+
+			con = DriverManager.getConnection(url, user, password);
+			st = con.createStatement();
+			// Add meeting to db
+			// Ex) 100000, Mec
+			String command = String.format(
+					"INSERT INTO meetings VALUES ('%d', '%s', '%s', '%s', '%s', '%s', '%s', '%s')", meetingId,
+					startTime, endTime, days.charAt(0), days.charAt(1), days.charAt(2), days.charAt(3), days.charAt(4));
+			st.executeUpdate(command);
+			st.close();
+
 		} catch (SQLException ex) {
 
 			Logger lgr = Logger.getLogger(Database.class.getName());
