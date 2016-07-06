add1bd90187610c7a1f24d1273a5b24dd54549fa
bfid111 <bfid111@gmail.com>
2016-07-06 15:19:58 -0400

    Fixed syntax error

--- sql/schedule.sql
+++ sql/schedule.sql
@@ -21,7 +21,7 @@ CREATE TABLE `meetings` (
   `W` bool null,
   `TR` bool null,
   `F` bool null,
-  PRIMARY KEY (`meeting id`),
+  PRIMARY KEY (`meeting id`)
 );
 
 /*classmeetings*/
@@ -30,6 +30,6 @@ CREATE TABLE `classmeetings` (
   `meeting id` int(64) NOT NULL,
   `course id` int(64) NOT NULL,
   FOREIGN KEY (`meeting id`) REFERENCES meetings(`meeting id`),
-  FOREIGN KEY (`course id`) REFERENCES classes(`course id`)
-  PRIMARY KEY (`meeting id`, `course id`),
+  FOREIGN KEY (`course id`) REFERENCES classes(`course id`),
+  PRIMARY KEY (`meeting id`, `course id`)
 );
