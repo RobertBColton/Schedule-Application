8c16e7fc5e154822a89416f7b7e74b14a3a217de
bfid111 <bfid111@gmail.com>
2016-07-07 16:55:01 -0400

    Added  loadData action

--- src/edu/psu/teamone/main/Controller.java
+++ src/edu/psu/teamone/main/Controller.java
@@ -1,25 +1,20 @@
 package edu.psu.teamone.main;
 
-import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
-
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Controller implements Initializable {
 	@FXML
@@ -27,10 +22,18 @@ public class Controller implements Initializable {
 	@FXML
 	private StackPane root;
 	@FXML
-	private TextField sectionName, sectionAbb, sectionId, startTime, endTime;
+	private TextField sectionName, sectionAbb, startTime, endTime;
 	// Class and Meeting
 	@FXML
 	private CheckBox dayMon, dayTues, dayWed, dayThur, dayFri;// MTWRF
+	@FXML
+	private TextField instructorName, instructorDiscipline, instructorId;
+	@FXML
+	private TextField editSectionId, editSectionName, editSectionAbb, editSectionStartTime, editSectionEndTime;
+	@FXML
+	private TableView sectionsTable;
+	@FXML
+	private TableColumn id, name, abbreviation, Time, Days;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -63,6 +66,16 @@ public class Controller implements Initializable {
 	}
 
 	@FXML
+	protected final void loadSections(ActionEvent event) {
+		//Load Data Button Action
+		// Loads all sections and meetings from db to arraylist
+		Database db = new Database();
+		ArrayList<Section> sections = new ArrayList<Section>();
+		ArrayList<Meeting> meetings = new ArrayList<Meeting>();
+		db.getDataFromDB(sections, meetings);
+	}
+
+	@FXML
 	protected final void addSectionAction(ActionEvent event) throws Exception {
 		boolean checkEmpty = false;
 		if (!dayMon.isSelected() && !dayTues.isSelected() && !dayWed.isSelected() && !dayThur.isSelected()
@@ -75,8 +88,8 @@ public class Controller implements Initializable {
 			alert.showAndWait();
 		}
 		if (sectionName.getText().trim().isEmpty() || sectionAbb.getText().trim().isEmpty()
-				|| sectionId.getText().trim().isEmpty() || this.startTime.getText().trim().isEmpty()
-				|| this.endTime.getText().trim().isEmpty()) {
+				|| this.startTime.getText().trim().isEmpty() || this.endTime.getText().trim().isEmpty()) {
+			// || sectionId.getText().trim().isEmpty()
 			checkEmpty = true;
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("Error");
@@ -100,8 +113,8 @@ public class Controller implements Initializable {
 			alert.setContentText("Cannot use only numbers for Course Abbreviation");
 			alert.showAndWait();
 		}
-		if (this.startTime.getText().matches(".*[a-zA-z]+.*") || this.endTime.getText().matches(".*[a-zA-z]+.*")
-				|| sectionId.getText().matches(".*[a-zA-z]+.*")) {
+		if (this.startTime.getText().matches(".*[a-zA-z]+.*") || this.endTime.getText().matches(".*[a-zA-z]+.*")) {
+			// || sectionId.getText().matches(".*[a-zA-z]+.*")
 			Alert alert = new Alert(AlertType.WARNING);
 			checkEmpty = true;
 			alert.setTitle("Error");
@@ -113,16 +126,17 @@ public class Controller implements Initializable {
 			// addClass inputs
 			String courseName = sectionName.getText();
 			String courseAbb = sectionAbb.getText();
-			int courseId = Integer.parseInt(sectionId.getText());
+			// int courseId = Integer.parseInt(sectionId.getText());
 			// addMeeting
-			int meetingId = courseId;
+			// int meetingId = courseId;
 			String startTime = this.startTime.getText() + ":00";
 			String endTime = this.endTime.getText() + ":00";
 			String days = (dayMon.isSelected() ? "1" : "0") + (dayTues.isSelected() ? "1" : "0")
 					+ (dayWed.isSelected() ? "1" : "0") + (dayThur.isSelected() ? "1" : "0")
 					+ (dayFri.isSelected() ? "1" : "0");
+			int courseId = db.generateId();
 			db.addClass(courseId, courseName, courseAbb);
-			db.addMeeting(meetingId, startTime, endTime, days);
+			db.addMeeting(courseId, startTime, endTime, days);
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.setTitle("Success");
 			alert.setContentText("Successfully Added a new course");
